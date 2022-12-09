4a20c4d03fec563ad1b4e9839304ac6b2a91d4ab
Mark Final <markfinal@hotmail.com>
2022-12-09 21:58:41 +0000

    Fixes #25. Reverting Python 3.11 claimed support, that was too soon
    Revert "Python 3.11: A test against 3.11-rc2 suggested it was working, so adding 3.11 to the version support"
    
    This reverts commit 94f2d292b35f53e056c12d1bd0c64c92d172afe5.

--- setup.py
+++ setup.py
@@ -153,9 +153,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.7, <3.12",
+    python_requires=">=3.7, <3.11",
     project_urls={
         'Documentation': 'https://cruiz.readthedocs.io/en/latest/',
         'GitHub': 'https://github.com/markfinal/cruiz',
