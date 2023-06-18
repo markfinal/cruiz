253cf9bc62f1468a1979d9b394f637fcdb7075fd
Mark Final <markfinal@hotmail.com>
2023-06-18 21:33:11 +0100

    Workers: Dynamically choose worker API based on conan version

--- cruiz/globals.py
+++ cruiz/globals.py
@@ -10,6 +10,8 @@ import typing
 
 CRUIZ_MAINWINDOW: typing.Optional[cruiz.MainWindow] = None  # type: ignore # noqa: F821
 
+CONAN_MAJOR_VERSION: int = 0
+
 
 def get_main_window() -> cruiz.MainWindow:  # type: ignore # noqa: F821
     """
@@ -17,3 +19,23 @@ def get_main_window() -> cruiz.MainWindow:  # type: ignore # noqa: F821
     """
     assert CRUIZ_MAINWINDOW
     return CRUIZ_MAINWINDOW
+
+
+def __capture_conan_version() -> None:
+    global CONAN_MAJOR_VERSION
+    if CONAN_MAJOR_VERSION > 0:
+        return
+
+    import subprocess
+
+    get_conan_version = subprocess.run(
+        ["conan", "--version"], capture_output=True, encoding="utf8", errors="ignore"
+    )
+    version = get_conan_version.stdout.replace("Conan version ", "").strip()
+    if not version:
+        raise ValueError("Unable to determine Conan version")
+    version_components = version.split(".")
+    CONAN_MAJOR_VERSION = int(version_components[0])
+
+
+__capture_conan_version()
