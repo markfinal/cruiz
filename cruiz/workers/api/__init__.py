37714702d38305dcab2ddc98323d4652c8774404
Mark Final <markfinal@hotmail.com>
2023-06-18 21:33:11 +0100

    Workers: Dynamically choose worker API based on conan version

--- cruiz/workers/api/__init__.py
+++ cruiz/workers/api/__init__.py
@@ -1,27 +1,32 @@
 # Cruiz workers API
 
-from .v1 import (
-    arbitrary,
-    build,
-    cmakebuildtool,
-    configinstall,
-    create,
-    deletecmakecache,
-    endmessagethread,
-    exportpackage,
-    imports,
-    install,
-    lockcreate,
-    meta,
-    package,
-    packagebinary,
-    packagedetails,
-    packagerevisions,
-    reciperevisions,
-    remotesearch,
-    removeallpackages,
-    removelocks,
-    removepackage,
-    source,
-    testpackage,
-)
+import cruiz.globals as globals
+
+if globals.CONAN_MAJOR_VERSION == 1:
+    from .v1 import (  # noqa: E402
+        arbitrary,
+        build,
+        cmakebuildtool,
+        configinstall,
+        create,
+        deletecmakecache,
+        endmessagethread,
+        exportpackage,
+        imports,
+        install,
+        lockcreate,
+        meta,
+        package,
+        packagebinary,
+        packagedetails,
+        packagerevisions,
+        reciperevisions,
+        remotesearch,
+        removeallpackages,
+        removelocks,
+        removepackage,
+        source,
+        testpackage,
+    )
+else:
+    pass
