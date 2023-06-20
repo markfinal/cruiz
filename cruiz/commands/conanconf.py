a6102f890073d8bcb7c22f63888877119cfa72f6
Mark Final <markfinal@hotmail.com>
2023-06-20 07:51:55 +0100

    Cache config: PRINT_RUN_COMMANDS is always on in Conan 2

--- cruiz/commands/conanconf.py
+++ cruiz/commands/conanconf.py
@@ -10,6 +10,13 @@ from enum import Enum
 class ConanConfigBoolean(Enum):
     """
     Enumeration for Conan configuration items that are Boolean values
+
+    Note that these appear to be Conan 1 specific
+
+    PRINT_RUN_COMMANDS is always on
+    (see https://github.com/conan-io/conan/issues/6752#issuecomment-1570619957)
+
+    REVISIONS are always on
     """
 
     PRINT_RUN_COMMANDS = "log.print_run_commands"
