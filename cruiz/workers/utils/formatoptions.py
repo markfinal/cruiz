343a104957def5a3c3312ef8488ad20e28ee5bf6
Mark Final <markfinal@hotmail.com>
2023-06-25 19:12:25 +0100

    Recipes: Creating lockfiles in memory for Conan v2
    - reusing an algorithm for the dependency graph from Conan v1
    - package_ids are still generated and queried - now has an explicit two stage approach in Conan 2

--- cruiz/workers/utils/formatoptions.py
+++ cruiz/workers/utils/formatoptions.py
@@ -9,7 +9,21 @@ import typing
 
 def format_options(options: typing.Dict[str, str]) -> typing.List[str]:
     """
-    Format the dictionary of options appropriately for Conan
+    Format the dictionary of options appropriately for Conan 1.x
     """
     options_list = [f"{optkey}={optvalue}" for optkey, optvalue in options.items()]
+    return options_list
+
+
+def format_options_v2(options: typing.Dict[str, str]) -> typing.List[str]:
+    """
+    Format the dictionary of options appropriately for Conan 2.x
+    Expected to be of the form
+        -o name/*:option=value
+    """
+    options_list: typing.List[str] = []
+    for optkey, optvalue in options.items():
+        assert ":" in optkey
+        optkey_split = optkey.split(":")
+        options_list.append(f"{optkey_split[0]}/*:{optkey_split[1]}={optvalue}")
     return options_list
