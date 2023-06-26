ec9827e6457e4e19c16c844879eea85fcd892f11
Mark Final <markfinal@hotmail.com>
2023-06-26 22:09:43 +0100

    Recipe: Adding the conan install command
    - although there is a Python API for install, it functions via a graph object,
    and we want to pass command line arguments here for now
    - may want to streamline that process in the future

---
+++ cruiz/workers/api/v2/install.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Install dependencies into the local cache
+"""
+
+from __future__ import annotations
+
+import multiprocessing
+
+from cruiz.interop.commandparameters import CommandParameters
+from cruiz.interop.message import Message, Success
+
+from . import worker
+
+
+def invoke(queue: multiprocessing.Queue[Message], params: CommandParameters) -> None:
+    """
+    Run 'conan install'
+    """
+    with worker.ConanWorker(queue, params) as api:
+        import importlib
+
+        # although there is a Python API for install, it works via a dependency
+        # graph, so use the CLI interface
+        imported_module = importlib.import_module("conan.cli.commands.install")
+        # strip the 'verb' from the front of the argument list
+        args = params.to_args()[1:]
+        imported_module.install.run(api, imported_module.install._parser, args)
+
+        queue.put(Success(None))
