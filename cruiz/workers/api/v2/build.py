b8a3359f71924fdaadc9d5730268f3b6126338dd
Mark Final <markfinal@hotmail.com>
2023-06-27 08:25:50 +0100

    Recipes: Implemented build method for Conan v2
    - there is the LocalAPI with a build method, but this took a fully configure
    conanfile object, e.g. as you might expect from after an install/graph generation
    - this was hard to piece together without a lot of support code, e.g. layout paths in particular
    - so just reached for the CLI build method and passed the arguments from the UI

---
+++ cruiz/workers/api/v2/build.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Build the source code of the package
+"""
+
+from __future__ import annotations
+
+import multiprocessing
+
+from cruiz.interop.commandparameters import CommandParameters
+from cruiz.interop.message import Message, Success
+
+from . import worker
+
+
+def invoke(queue: multiprocessing.Queue[Message], params: CommandParameters) -> None:
+    """
+    Run 'conan build'
+    """
+    with worker.ConanWorker(queue, params) as api:
+        import importlib
+
+        # although there is a Python API for build, the setup was complex, as it
+        # expected a fully configured conanfile object (e.g. for paths, layouts etc)
+        # so reach for the CLI method
+        imported_module = importlib.import_module("conan.cli.commands.build")
+        # strip the 'verb' from the front of the argument list
+        args = params.to_args()[1:]
+        imported_module.build.run(api, imported_module.build._parser, args)
+
+        queue.put(Success(None))
