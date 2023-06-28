c848e22cab8ddc94df0d2c375bbd1d0c3bd7ca87
Mark Final <markfinal@hotmail.com>
2023-06-28 20:36:53 +0100

    Recipes: Implement test worker for Conan v2
    - again uses the CLI api
    - conan test in Conan 2 seems to be one of the few CLI commands that needs a package reference again
    so there is additional logic to tend for that

---
+++ cruiz/workers/api/v2/testpackage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Worker implementation for testing packages
+"""
+
+from __future__ import annotations
+
+import multiprocessing
+
+from cruiz.interop.commandparameters import CommandParameters
+from cruiz.interop.message import Message, Success
+
+from . import worker
+
+
+def invoke(queue: multiprocessing.Queue[Message], params: CommandParameters) -> None:
+    """
+    Run 'conan test'
+    """
+    with worker.ConanWorker(queue, params) as api:
+        import importlib
+
+        # although there is a Python API for test, it works via a dependency
+        # graph, so use the CLI interface
+        imported_module = importlib.import_module("conan.cli.commands.test")
+        # strip the 'verb' from the front of the argument list
+        args = params.to_args()[1:]
+        imported_module.test.run(api, imported_module.test._parser, args)
+
+        queue.put(Success(None))
