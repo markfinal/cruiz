a349008a2d9acdfe2ea98d3b42c562a63fd723d1
Mark Final <markfinal@hotmail.com>
2023-06-29 06:42:09 +0100

    Recipes: Support change in Conan v2.0.7
    Between 2.0.6 and 2.0.7, there was a bug fix in Conan that addressed the
    use of argument parsers. This had a knock-on effect in the API that cruiz
    was using, so this is a breaking change for cruiz.

--- cruiz/workers/api/v2/install.py
+++ cruiz/workers/api/v2/install.py
@@ -26,6 +26,6 @@ def invoke(queue: multiprocessing.Queue[Message], params: CommandParameters) -> 
         imported_module = importlib.import_module("conan.cli.commands.install")
         # strip the 'verb' from the front of the argument list
         args = params.to_args()[1:]
-        imported_module.install.run(api, imported_module.install._parser, args)
+        imported_module.install.run(api, args)
 
         queue.put(Success(None))
