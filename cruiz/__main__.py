952016018fbf4734854768ddd0710669b7f1cae5
Mark Final <markfinal@hotmail.com>
2023-07-15 14:04:46 +0100

    Fixes #98. Linting. Generate the UI resources before linting
    - resource generation occurs as the very first thing when cruiz starts now
    - this avoids needing any graphical resources on CI machines

--- cruiz/__main__.py
+++ cruiz/__main__.py
@@ -6,6 +6,12 @@ cruiz: Conan recipe user interface
 
 import os
 
+from cruiz.resourcegeneration import generate_resources
+
+# resource generation be invoked before resources and MainWindow are imported
+generate_resources()
+
+
 os.environ.setdefault("QT_API", "pyside6")
 
 from cruiz.entrypoint import main  # noqa: E402
