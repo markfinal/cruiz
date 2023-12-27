2fd7af1f833ea9a76639744d6bfd58f1516e0a77
Mark Final <markfinal@hotmail.com>
2023-12-27 21:49:26 +0000

    Fixes #133 Support Python 3.12

--- setup.py
+++ setup.py
@@ -152,8 +152,9 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.8, <3.12",
+    python_requires=">=3.8, <3.13",
     project_urls={
         "Documentation": "https://cruiz.readthedocs.io/en/latest/",
         "GitHub": "https://github.com/markfinal/cruiz",
