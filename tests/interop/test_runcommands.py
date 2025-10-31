8d6bd2e2e60eb7703ce48b65a74b20d8ab79290b
Mark Final <markfinal@hotmail.com>
2025-10-31 22:25:54 +0000

    Tests: Fix EOLs for Windows

--- tests/interop/test_runcommands.py
+++ tests/interop/test_runcommands.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import os
 import typing
 
 from cruizlib.runcommands import (
@@ -15,19 +16,22 @@ if typing.TYPE_CHECKING:
     import pytest_subprocess
 
 
+EOL = "\r\n" if os.name == "nt" else "\n"
+
+
 def test_run(fake_process: pytest_subprocess.FakeProcess) -> None:
     """Exercise the run function."""
     fake_process.register(["git", "branch"], stdout=["* fake_branch", "  main"])
     process = run(["git", "branch"], capture_output=True)
     assert process.returncode == 0
-    assert process.stdout == "* fake_branch\n  main\n"
+    assert process.stdout == f"* fake_branch{EOL}  main{EOL}"
 
 
 def test_run_for_output(fake_process: pytest_subprocess.FakeProcess) -> None:
     """Exercise the run_get_output function."""
     fake_process.register(["git", "branch"], stdout=["* fake_branch", "  main"])
     output = run_get_output(["git", "branch"])
-    assert output == "* fake_branch\n  main\n"
+    assert output == f"* fake_branch{EOL}  main{EOL}"
 
 
 def test_run_for_combined_output(fake_process: pytest_subprocess.FakeProcess) -> None:
@@ -38,7 +42,7 @@ def test_run_for_combined_output(fake_process: pytest_subprocess.FakeProcess) ->
         stderr=["There is no error"],
     )
     output = run_get_combined_output(["git", "branch"])
-    assert output == "* fake_branch\n  main\nThere is no error\n"
+    assert output == f"* fake_branch{EOL}  main{EOL}There is no error{EOL}"
 
 
 def test_run_popen(fake_process: pytest_subprocess.FakeProcess) -> None:
@@ -46,5 +50,5 @@ def test_run_popen(fake_process: pytest_subprocess.FakeProcess) -> None:
     fake_process.register(["git", "branch"], stdout=["* fake_branch", "  main"])
     with get_popen_for_capture(["git", "branch"]) as process:
         assert process.stdout
-        assert process.stdout.readline() == "* fake_branch\n"
-        assert process.stdout.readline() == "  main\n"
+        assert process.stdout.readline() == f"* fake_branch{EOL}"
+        assert process.stdout.readline() == f"  main{EOL}"
