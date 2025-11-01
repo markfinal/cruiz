b579cbd1fb9a39aa9274cd515824dc9110d0ad4c
Mark Final <markfinal@hotmail.com>
2025-11-01 06:50:52 +0000

    Tests: Make single process command runner into a fixture

--- tests/workers/conftest.py
+++ tests/workers/conftest.py
@@ -1,21 +1,45 @@
 """Common fixtures for workers."""
 
+import contextlib
+import logging
 import multiprocessing
+import queue
+import threading
 import typing
 
 import cruizlib.workers.api as workers_api
 from cruizlib.interop.commandparameters import CommandParameters
-from cruizlib.interop.message import End
+from cruizlib.interop.message import (
+    ConanLogMessage,
+    End,
+    Failure,
+    Message,
+    Stderr,
+    Stdout,
+    Success,
+)
 
 # pylint: disable=wrong-import-order
 import pytest
+
+LOGGER = logging.getLogger(__name__)
 
 
 # TODO: unable to write the return type I'm interested in, as Python keeps erroring
 # see where this fixture is used for the type intented
 @pytest.fixture()
 def meta() -> typing.Generator[typing.Tuple[typing.Any, typing.Any], None, None]:
-    """Fixture for setup and teardown of meta processes and queues."""
+    """
+    Fixture for setup and teardown of meta processes and queues.
+
+    On the test process, a request and reply queue are created.
+    And then a subprocess running the worker is started.
+
+    On teardown, the request queue is finished, and joined.
+    Then the responses are joined.
+
+    Finally the subprocess is joined.
+    """
     params = CommandParameters("meta", workers_api.meta.invoke)
     context = multiprocessing.get_context("spawn")
     request_queue = context.JoinableQueue()
@@ -44,3 +68,51 @@ def meta() -> typing.Generator[typing.Tuple[typing.Any, typing.Any], None, None]
     # wait for the child process to finish
     process.join()
     process.close()
+
+
+@pytest.fixture()
+def single_process_command_runner(
+    request: pytest.FixtureRequest,
+) -> typing.Generator[typing.List[Message], None, None]:
+    """
+    Fixture for running a command in the current process.
+
+    A queue is spawned on a thread.
+    The worker is executed on the test thread.
+    Success/Fail responses are returned from the fixture.
+    Logging responses are sent to the logger.
+    """
+
+    def _reply_watcher(
+        reply_queue: queue.Queue[Message], replies: typing.List[Message]
+    ) -> None:
+        while True:
+            reply = reply_queue.get()
+            if isinstance(reply, (Success, Failure)):
+                replies.append(reply)
+                break
+            if isinstance(reply, (ConanLogMessage, Stdout, Stderr)):
+                LOGGER.info("Message: '%s'", reply.message)
+                continue
+            raise ValueError(f"Unknown reply of type '{type(reply)}'")
+
+    @contextlib.contextmanager
+    def _reply_queue_manager(
+        reply_queue: queue.Queue[Message],
+    ) -> typing.Generator[typing.List[Message], None, None]:
+        replies: typing.List[Message] = []
+        watcher_thread = threading.Thread(
+            target=_reply_watcher, args=(reply_queue, replies)
+        )
+        watcher_thread.start()
+        yield replies
+        watcher_thread.join()
+
+    cmd_name, worker = request.param
+    params = CommandParameters(cmd_name, worker)
+    reply_queue = queue.Queue[Message]()
+    with _reply_queue_manager(reply_queue) as replies:
+        # abusing the type system, as the API used for queue.Queue is the same
+        # as for multiprocessing.Queue
+        worker(reply_queue, params)
+    yield replies
--- tests/workers/test_conan_command_single_process.py
+++ tests/workers/test_conan_command_single_process.py
@@ -8,63 +8,27 @@ added complexity.
 
 from __future__ import annotations
 
-import contextlib
 import logging
-import queue
-import threading
 import typing
 
 import cruizlib.workers.api as workers_api
-from cruizlib.interop.commandparameters import CommandParameters
-from cruizlib.interop.message import (
-    ConanLogMessage,
-    Failure,
-    Message,
-    Stderr,
-    Stdout,
-    Success,
-)
+from cruizlib.interop.message import Failure, Message
+
+import pytest
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def _queue_watcher_local(
-    reply_queue: queue.Queue[Message], replies: typing.List[Message]
+@pytest.mark.parametrize(
+    "single_process_command_runner",
+    [("install", workers_api.install.invoke)],
+    indirect=["single_process_command_runner"],
+)
+def test_expected_failure(
+    single_process_command_runner: typing.List[Message],
 ) -> None:
-    while True:
-        reply = reply_queue.get()
-        if isinstance(reply, (Success, Failure)):
-            replies.append(reply)
-            break
-        if isinstance(reply, (ConanLogMessage, Stdout, Stderr)):
-            LOGGER.info("Message: '%s'", reply.message)
-            continue
-        raise ValueError(f"Unknown reply of type '{type(reply)}'")
-
-
-@contextlib.contextmanager
-def _watcher_local(
-    reply_queue: queue.Queue[Message],
-) -> typing.Generator[typing.List[Message], None, None]:
-    replies: typing.List[Message] = []
-    watcher_thread = threading.Thread(
-        target=_queue_watcher_local, args=(reply_queue, replies)
-    )
-    watcher_thread.start()
-    yield replies
-    watcher_thread.join()
-
-
-def test_expected_failure_single_process() -> None:
     """Test: running conan install incorrect setup, so has an expected failure."""
-    worker = workers_api.install.invoke
-    params = CommandParameters("install", worker)
-
-    reply_queue = queue.Queue[Message]()
-    with _watcher_local(reply_queue) as replies:
-        # abusing the type system, as the API used for queue.Queue is the same
-        # as for multiprocessing.Queue
-        worker(reply_queue, params)  # type: ignore[arg-type]
+    replies = single_process_command_runner
     assert replies
     assert isinstance(replies[0], Failure)
