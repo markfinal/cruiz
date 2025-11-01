6bd7d1c7fb04dc697f8d22cb01548b58dfd68cde
Mark Final <markfinal@hotmail.com>
2025-11-01 23:06:40 +0000

    Tests: Exercising all optional arguments to conan install. Conan2 --update is skipped
    The skip is because
    
    conan install <path/to/recipe>
    
    works but
    
    conan install --update <path/to/recipe>
    
    only looks in the current directory. The docs suggest this behaviour, but not based on --update

--- tests/workers/test_conan_command_single_process.py
+++ tests/workers/test_conan_command_single_process.py
@@ -46,12 +46,37 @@ def test_expected_failure(
         watcher_thread.join()
 
 
+@pytest.mark.parametrize(
+    "arg,value",
+    [
+        (None, None),
+        ("version", "1.0"),
+        ("install_folder", "install"),
+        ("options", {"shared": "True"}),
+        (("user", "channel"), ("user1", "channel1")),
+        # (conan2) conan install <path> works but conan install --update <path>
+        # defaults to the current directory - there is documentation saying this
+        # happens, but not for adding --update
+        pytest.param(
+            "arguments",
+            ["--update"],
+            marks=pytest.mark.xfail(
+                CONAN_MAJOR_VERSION == 2,
+                reason="Unexpected Conan 2 install behaviour with --update",
+            ),
+        ),
+    ],
+)
+# pylint: disable=too-many-arguments, too-many-positional-arguments
 def test_conan_install(
     reply_queue_fixture: typing.Tuple[
         queue.Queue[Message], typing.List[Message], threading.Thread
     ],
     conan_recipe: pathlib.Path,
     conan_local_cache: typing.Dict[str, str],
+    tmp_path: pathlib.Path,
+    arg: typing.Optional[str],
+    value: typing.Union[typing.Optional[str], typing.List[str], typing.Dict[str, str]],
 ) -> None:
     """Test: running conan install."""
     worker = workers_api.install.invoke
@@ -60,6 +85,24 @@ def test_conan_install(
     params.recipe_path = conan_recipe
     params.cwd = conan_recipe.parent
     params.profile = "default"
+    if arg:
+        if isinstance(arg, str):
+            if arg == "arguments":
+                assert isinstance(value, list)
+                params.arguments.extend(value)
+            elif arg == "options":
+                assert isinstance(value, dict)
+                for key, val in value.items():
+                    params.add_option("test", key, val)
+            elif arg == "install_folder":
+                assert isinstance(value, str)
+                params.install_folder = tmp_path / value
+            else:
+                setattr(params, arg, value)
+        else:
+            assert isinstance(arg, tuple)
+            for index, key in enumerate(arg):
+                setattr(params, key, value[index])
     reply_queue, replies, watcher_thread = reply_queue_fixture
     # abusing the type system, as the API used for queue.Queue is the same
     # as for multiprocessing.Queue
@@ -69,4 +112,8 @@ def test_conan_install(
     assert replies
     assert isinstance(replies[0], Success)
     if CONAN_MAJOR_VERSION == 1:
-        assert (conan_recipe.parent / "conan.lock").exists()
+        if arg == "install_folder":
+            assert isinstance(value, str)
+            assert (tmp_path / value / "conan.lock").exists()
+        else:
+            assert (conan_recipe.parent / "conan.lock").exists()
