c862b179c54b305a89a51299ba593bd061566ae0
Mark Final <markfinal@hotmail.com>
2025-11-01 16:58:34 +0000

    Test: Rewrote fixture for multiprocess command, so thht the CommandParameters can be tweaked in the test

--- tests/workers/test_conan_commands.py
+++ tests/workers/test_conan_commands.py
@@ -1,20 +1,37 @@
 """Test the Conan command functionality, using multiple processes."""
 
+from __future__ import annotations
+
+import threading
 import typing
 
 import cruizlib.workers.api as workers_api
+from cruizlib.interop.commandparameters import CommandParameters
 from cruizlib.interop.message import Failure, Message
 
-import pytest
+if typing.TYPE_CHECKING:
+    from cruizlib.multiprocessingmessagequeuetype import (
+        MultiProcessingMessageQueueType,
+    )
 
 
-@pytest.mark.parametrize(
-    "multi_process_command_runner",
-    [("install", workers_api.install.invoke)],
-    indirect=["multi_process_command_runner"],
-)
-def test_expected_failure(multi_process_command_runner: typing.List[Message]) -> None:
+def test_expected_failure(
+    multiprocess_reply_queue_fixture: typing.Tuple[
+        MultiProcessingMessageQueueType,
+        typing.List[Message],
+        threading.Thread,
+        typing.Any,
+    ],
+) -> None:
     """Test: running conan install incorrect setup, so has an expected failure."""
-    replies = multi_process_command_runner
+    worker = workers_api.install.invoke
+    params = CommandParameters("install", worker)
+    reply_queue, replies, watcher_thread, context = multiprocess_reply_queue_fixture
+
+    process = context.Process(target=worker, args=(reply_queue, params))
+    process.start()
+    process.join()
+    watcher_thread.join()
+
     assert replies
     assert isinstance(replies[0], Failure)
