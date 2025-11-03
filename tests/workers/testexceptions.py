4869a355ac8a0a1abf061261aeea99900fd3f9f7
Mark Final <markfinal@hotmail.com>
2025-11-03 08:05:17 +0000

    Tests: Add a timeout for the Python Thread watching for message replies

--- tests/workers/testexceptions.py
+++ tests/workers/testexceptions.py
@@ -3,3 +3,7 @@
 
 class FailedMessageTestError(Exception):
     """Exception when a Failure reply is encountered."""
+
+
+class WatcherThreadTimeoutError(Exception):
+    """Exception when the watcher thread has timed out."""
