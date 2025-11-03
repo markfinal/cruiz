166a9897b36a8075eb6f2776d793ab7b75ff8a57
Mark Final <markfinal@hotmail.com>
2025-11-03 21:41:12 +0000

    Tests: Add meta remote sync.
    The encoding of the request with parameters should really be reused.

--- tests/workers/test_meta.py
+++ tests/workers/test_meta.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import logging
 import typing
+import urllib.parse
 
 from cruizlib.globals import CONAN_FULL_VERSION, CONAN_MAJOR_VERSION
 from cruizlib.interop.message import (
@@ -14,6 +15,7 @@ from cruizlib.interop.message import (
     Stdout,
     Success,
 )
+from cruizlib.interop.pod import ConanRemote
 
 # pylint: disable=wrong-import-order
 import pytest
@@ -95,5 +97,47 @@ def test_meta_get_remotes_list(
 
     assert isinstance(reply, Success)
     assert isinstance(reply.payload, list)
+
+    _meta_done(request_queue, reply_queue)
+
+
+def test_meta_remotes_sync(
+    meta: typing.Tuple[
+        MultiProcessingStringJoinableQueueType, MultiProcessingMessageQueueType
+    ],
+) -> None:
+    """Via the meta worker: Synchronize remotes."""
+    request_queue, reply_queue = meta
+
+    def _get_remotes() -> typing.List[ConanRemote]:
+        request_queue.put("remotes_list")
+
+        reply = _process_replies(reply_queue)
+        assert isinstance(reply, Success)
+        return reply.payload
+
+    initial_remotes = _get_remotes()
+    assert len(initial_remotes) == 1
+
+    def _sync_new_remotes() -> None:
+        new_remotes = [
+            ConanRemote("ARemote", "http://a.remote.com", True),
+            ConanRemote("BRemote", "http://b.remote.com", False),
+        ]
+        payload = {"remotes": new_remotes}
+        remotes_sync_request = (
+            f"remotes_sync?{urllib.parse.urlencode(payload, doseq=True)}"
+        )
+
+        request_queue.put(remotes_sync_request)
+
+        reply = _process_replies(reply_queue)
+        assert isinstance(reply, Success)
+        assert reply.payload is None
+
+    _sync_new_remotes()
+
+    syncd_remotes = _get_remotes()
+    assert len(syncd_remotes) == 2
 
     _meta_done(request_queue, reply_queue)
