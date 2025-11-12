d2397d1ab75a50bc0015859d4c584b83589ff6d4
Mark Final <markfinal@hotmail.com>
2025-11-12 21:52:41 +0000

    Refactor: Get Conan version as componentised integers

--- src/cruizlib/globals.py
+++ src/cruizlib/globals.py
@@ -10,13 +10,15 @@ import importlib.metadata
 import typing
 
 
-def __capture_conan_version() -> typing.Tuple[str, int]:
+def __capture_conan_version() -> typing.Tuple[str, int, typing.Tuple[int, ...]]:
     full_version = importlib.metadata.version("conan")
 
-    version_components = full_version.split(".")
-    major_version = int(version_components[0])
+    version_components = tuple(map(int, full_version.split(".")))
+    major_version = version_components[0]
 
-    return full_version, major_version
+    return (full_version, major_version, version_components)
 
 
-CONAN_FULL_VERSION, CONAN_MAJOR_VERSION = __capture_conan_version()
+CONAN_FULL_VERSION, CONAN_MAJOR_VERSION, CONAN_VERSION_COMPONENTS = (
+    __capture_conan_version()
+)
