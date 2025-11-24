e5ee8043cd8e8bb39e69ee5f4405555f4beaea22
Mark Final <markfinal@hotmail.com>
2025-11-24 06:52:00 +0000

    Tests: Add test for searching for recipe revisions remotely
    Conan 2 does not allow revisions to be disabled.
    
    Conan 1.17.1 cannot connect to Conan Center so will fail.

---
+++ tests/workers/single_process/test_conan_remote_recipe_revisions.py
@@ -0,0 +1,83 @@
+"""
+Test the conan remote recipe revision search functionality, in a single process.
+
+Note that this is not how cruiz tends to work, and should be using multiple processes
+to isolate the Conan commands, but this test shows it still works without that
+added complexity.
+"""
+
+import logging
+import queue
+import threading
+import typing
+from contextlib import nullcontext as does_not_raise
+
+
+import cruizlib.workers.api as workers_api
+from cruizlib.globals import CONAN_MAJOR_VERSION, CONAN_VERSION_COMPONENTS
+from cruizlib.interop.message import (
+    Message,
+    Success,
+)
+from cruizlib.interop.reciperevisionsparameters import RecipeRevisionsParameters
+
+# pylint: disable=wrong-import-order
+import pytest
+
+# pylint: disable=import-error
+import testexceptions
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize(
+    "envvars,expectation",
+    [
+        pytest.param(
+            {"CONAN_REVISIONS_ENABLED": "0"},
+            pytest.raises(testexceptions.FailedMessageTestError),
+            marks=pytest.mark.xfail(
+                CONAN_MAJOR_VERSION == 2,
+                reason="Conan 2 does not allow disabling revisions",
+            ),
+        ),
+        pytest.param(
+            {"CONAN_REVISIONS_ENABLED": "1"},
+            does_not_raise(),
+            marks=pytest.mark.xfail(
+                CONAN_VERSION_COMPONENTS == (1, 17, 1),
+                reason="Conan 1.17.1 cannot connect",
+            ),
+        ),
+    ],
+)
+def test_conan_remote_rrev_search(
+    reply_queue_fixture: typing.Callable[
+        [], typing.Tuple[queue.Queue[Message], typing.List[Message], threading.Thread]
+    ],
+    conan_local_cache: typing.Dict[str, str],
+    envvars: typing.Dict[str, str],
+    expectation: typing.Iterator[None],
+) -> None:
+    """Test: running conan remote rrev searches for a given package."""
+    worker = workers_api.reciperevisions.invoke
+    params = RecipeRevisionsParameters(
+        reference="zlib/1.3.1",
+        remote_name="conancenter",
+    )
+    params.added_environment = conan_local_cache
+    params.added_environment.update(envvars)
+    reply_queue, replies, watcher_thread = reply_queue_fixture()
+    # abusing the type system, as the API used for queue.Queue is the same
+    # as for multiprocessing.Queue
+    with expectation:  # type: ignore[attr-defined]
+        worker(reply_queue, params)  # type: ignore[arg-type]
+        watcher_thread.join(timeout=5.0)
+        if watcher_thread.is_alive():
+            raise testexceptions.WatcherThreadTimeoutError()
+
+        assert replies
+        assert isinstance(replies[0], Success)
+        assert isinstance(replies[0].payload, list)
+        assert replies[0].payload
