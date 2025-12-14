40c1a2ab5aad4144057f457690f52c051ba22063
Mark Final <markfinal@hotmail.com>
2025-12-14 16:35:24 +0000

    Fix

--- src/cruizlib/multiprocessingmessagequeuetype.py
+++ src/cruizlib/multiprocessingmessagequeuetype.py
@@ -3,18 +3,19 @@
 """Type annotation for multiprocssing Queues on Messages."""
 
 import multiprocessing
-import multiprocessing.queues
+
+# import multiprocessing.queues
 
 from cruizlib.interop.message import Message
 
-try:
-    # multiprocessing.Queue is a function rather than a class, so it does not satisfy
-    # the usual static type rules
-    # multiprocessing.queues is undocumented in more recent Pythons
-    # see https://github.com/python/cpython/issues/99509#issuecomment-1742069772
-    MultiProcessingMessageQueueType = multiprocessing.queues.Queue[Message]
-    MultiProcessingStringJoinableQueueType = multiprocessing.queues.JoinableQueue[str]
-except TypeError:
-    # pylint: disable=unsubscriptable-object
-    MultiProcessingMessageQueueType = multiprocessing.Queue[Message]
-    MultiProcessingStringJoinableQueueType = multiprocessing.JoinableQueue[str]
+# multiprocessing.Queue is a function rather than a class, so it does not satisfy
+# the usual static type rules
+# multiprocessing.queues is undocumented in more recent Pythons
+# see https://github.com/python/cpython/issues/99509#issuecomment-1742069772
+# unfortunately canot do this while older Pythons (at least 3.10) are being tested
+# MultiProcessingMessageQueueType = multiprocessing.queues.Queue[Message]
+# MultiProcessingStringJoinableQueueType = multiprocessing.queues.JoinableQueue[str]
+
+# pylint: disable=unsubscriptable-object
+MultiProcessingMessageQueueType = multiprocessing.Queue[Message]
+MultiProcessingStringJoinableQueueType = multiprocessing.JoinableQueue[str]
